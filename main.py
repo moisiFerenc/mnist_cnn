d20b17f32e04edf3934c1297fb93fa834716115d
moisiFerenc <varazsferenc@Gmail.com>
2023-10-11 17:11:57 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# ...
+
+# Importáljuk a szükséges könyvtárakat és modulokat
 import numpy as np
 import pandas as pd
 import random
@@ -6,24 +9,32 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Load the MNIST dataset from a CSV file
+# Az MNIST adathalmaz betöltése egy CSV fájlból
 mnist_data = pd.read_csv('train_data/mnist_train.csv')
-mnist_data = mnist_data.sample(frac=1).reset_index(drop=True)  # Shuffle the data
-X = mnist_data.iloc[:, 1:].values
-Y = mnist_data.iloc[:, 0].values
-X = X / 255.0  # Normalize pixel values to the range [0, 1]
 
-# Split the data into training and validation sets
-split_ratio = 0.9
+# Az adatokat véletlenszerűen összekeverjük, hogy ne legyen minta sorrendi hatás
+mnist_data = mnist_data.sample(frac=1).reset_index(drop=True)
+
+# Az X változóba helyezzük az adatok képeket (pixel értékeket)
+X = mnist_data.iloc[:, 1:].values
+
+# Az Y változóba helyezzük az adatok címkéit (osztályokat)
+Y = mnist_data.iloc[:, 0].values
+
+# A pixel értékeket normalizáljuk, hogy a tartomány [0, 1] legyen
+X = X / 255.0
+
+# Az adatokat felosztjuk tréning és validációs halmazra
+split_ratio = 0.9  # Az adatok 90%-a tréninghalmazban lesz
 split_index = int(split_ratio * len(X))
 X_train, X_dev = X[:split_index], X[split_index:]
 Y_train, Y_dev = Y[:split_index], Y[split_index:]
 
-# Reshape the data for convolutional network
+# Az adatokat átalakítjuk, hogy megfeleljenek a konvolúciós hálózat számára
 X_train = X_train.reshape(-1, 28, 28, 1)
 X_dev = X_dev.reshape(-1, 28, 28, 1)
 
-# Define a simple CNN model
+# Egyszerű konvolúciós modell létrehozása
 def create_cnn_model():
     model = keras.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
@@ -35,18 +46,18 @@ def create_cnn_model():
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Training function using the CNN
+# Az adatok tréningje a konvolúciós hálózaton
 def train_cnn(X_train, Y_train, epochs):
     model = create_cnn_model()
     model.fit(X_train, Y_train, epochs=epochs, validation_data=(X_dev, Y_dev))
     return model
 
-# Function to evaluate the accuracy of the CNN
+# A konvolúciós modell pontosságának kiértékelése
 def evaluate_cnn(model, X, Y):
     _, accuracy = model.evaluate(X, Y, verbose=0)
     return accuracy
 
-# Function to make predictions using the CNN
+# Az eredmények megjelenítésére és tesztelésre szolgáló funkció
 def test_cnn_prediction(index, model):
     current_image = X_dev[index].reshape(28, 28) * 255
     plt.gray()
@@ -59,46 +70,46 @@ def test_cnn_prediction(index, model):
     print("Prediction:", predicted_label)
     print("True Label:", true_label)
 
-# Modify the 'menu' function to train and test the CNN
+# A menü funkció módosítása a konvolúciós hálózat tréningjére és tesztelésére
 def menu_cnn():
     model = None
     while True:
-        print("*********Menu*********\n")
-        print("1. Train CNN\n")
-        print("2. Test CNN\n")
-        print("3. Accuracy\n")
-        print("4. Exit\n")
-        print("Select an option:\n")
+        print("*********Menü*********\n")
+        print("1. CNN tréning\n")
+        print("2. CNN teszt\n")
+        print("3. Pontosság\n")
+        print("4. Kilépés\n")
+        print("Válasszon egy lehetőséget:\n")
 
         choice = input()
 
         if choice == '1':
-            print("Option 1. \n Input epochs:")
+            print("1. lehetőség. Kérem adja meg az epoch-ok számát:")
             epochs = int(input())
             model = train_cnn(X_train, Y_train, epochs)
 
         elif choice == '2':
             if model is not None:
-                print("Option 2. Testing CNN")
+                print("2. lehetőség. CNN tesztelése")
                 index = random.randint(0, len(X_dev) - 1)
                 test_cnn_prediction(index, model)
             else:
-                print("Please train the CNN first.")
+                print("Kérem először tréningezze a CNN-t.")
 
         elif choice == '3':
             if model is not None:
-                print("Option 3. Accuracy:")
+                print("3. lehetőség. Pontosság kiértékelése:")
                 accuracy = evaluate_cnn(model, X_dev, Y_dev)
-                print("Accuracy: {:.2f}%".format(accuracy * 100))
+                print("Pontosság: {:.2f}%".format(accuracy * 100))
             else:
-                print("Please train the CNN first.")
+                print("Kérem először tréningezze a CNN-t.")
 
         elif choice == '4':
-            print("Exiting the program.")
+            print("Kilépés a programból.")
             break
 
         else:
-            print("Invalid choice. Please select a valid option.")
+            print("Érvénytelen választás. Kérem válasszon egy érvényes lehetőséget.")
 
-# Call the modified menu function
+# Hívja meg a módosított menü funkciót
 menu_cnn()
